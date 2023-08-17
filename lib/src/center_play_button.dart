988d799b33ff911cf188603a4a20703f60c3c47c
dieofcode <fr0stsone878@gmail.com>
2023-08-17 10:03:58 +0400

    change icon

--- lib/src/center_play_button.dart
+++ lib/src/center_play_button.dart
@@ -39,7 +39,7 @@ class CenterPlayButton extends StatelessWidget {
                 iconSize: 32,
                 padding: const EdgeInsets.all(12.0),
                 icon: isFinished
-                    ? Icon(Icons.replay, color: iconColor)
+                    ? Icon(Icons.play_arrow_rounded, color: iconColor)
                     : AnimatedPlayPause(
                         color: iconColor,
                         playing: isPlaying,
